251e5de2282828a0c9326210496bff8a390ebefa
lucaspauloop <lucaspaulodeoliveira@outlook.com>
2019-06-26 11:45:38 -0300

    graphcs of user

--- src/app/container/user/userinicial/userinicial.component.ts
+++ src/app/container/user/userinicial/userinicial.component.ts
@@ -5,6 +5,11 @@ import {Service} from '../../../service.service';
 import {User} from '../user-schema';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
+import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
+import { getStyle, hexToRgba } from '@coreui/coreui/dist/js/coreui-utilities';
+import { count } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import {ChartsModule} from 'ng2-charts'
 
 @Component({
   selector: 'app-user',
@@ -19,6 +24,20 @@ export class UserinicialComponent implements OnInit {
 
   constructor(private router: Router, private service: Service, private formBuilder: FormBuilder) { }
 
+  // barChart
+  public barChartOptions: any = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  public barChartLabels: string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType = 'bar';
+  public barChartLegend = true;
+
+  public barChartData: any[] = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  ];
+  
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
   ngOnInit() {
--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -4,35 +4,58 @@ import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
 import {Service} from '../../service.service';
 import {User} from '../../container/user/user-schema';
 import { Router } from '@angular/router';
+import { count } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import {Student} from '../../container/student/student-schema'
+import {Course} from '../../container/course/course-schema'
 
 @Component({
   templateUrl: 'dashboard.component.html'
 })
 
 export class DashboardComponent implements OnInit {
+  /*graphcs User*/
   constructor( private service: Service) { }
+  total:number
+  dataSourceGraphcs:number;
+  user :User[];
+  admin=0;
+  guess=0;
 
-  dataSource:User[];
-  user : User[];
-  num = 0;
-
-  getUser(){
-    this.service.getAllUsers().subscribe(res=>{
-      this.dataSource=res;
-      console.log(this.dataSource);
+  getUser() {
+    this.service.getAllUsers().subscribe((res: User[]) =>{
+      res.forEach(element => {
+        if(element.profile == 'admin')
+        {
+            this.admin++;
+        }
+        else
+        if(element.profile == 'guess')
+        {
+          this.guess++;
+        }
+      });
+      this.barChart1Data = [
+        {
+          data: [this.admin,0],
+          label: 'administrador',
+        },
+        {
+          data: [this.guess,0],
+          label: 'Convidados',
+        }
+      ];
+      this.total = this.admin + this.guess
+      console.log(this.dataSourceGraphcs);
+      return this.dataSourceGraphcs;
       })
-      this.num = this.dataSource.length
   }
 
+  /*grphcs Student*/
+ 
   // barChart1
-  public barChart1Data: Array<any> = [
-    {
-     
-      data: [11,12,0],
-      label: 'Usuário'
-    }
-  ];
-  public barChart1Labels: Array<any> = ['Administrador', 'Convidado'];
+  public barChart1Data: Array<any> = [];
+  public barChart1Labels: Array<any> = ['Usuários'];
   public barChart1Options: any = {
     tooltips: {
       enabled: false,
@@ -54,7 +77,11 @@ export class DashboardComponent implements OnInit {
   };
   public barChart1Colours: Array<any> = [
     {
-      backgroundColor: 'rgba(255,255,255)',
+      backgroundColor: 'rgba(84,255,87)',
+      borderWidth: 0
+    },
+    {
+      backgroundColor: 'rgba(255,237,14)',
       borderWidth: 0
     }
   ];
@@ -64,8 +91,9 @@ export class DashboardComponent implements OnInit {
  
 
   ngOnInit(): void {
+    console.log(this.dataSourceGraphcs);
     // generate random values for mainChart
-  
+      this.getUser();
     }
   
 }
